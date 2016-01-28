f87a91309b465d37cc3f42274438e62be18b8e44
Pablo L <pabsormar@users.noreply.github.com>
2016-01-28 23:30:36 +0100

    First commit of the project 'GUIDesign' following IntelliJ structure.

---
+++ GUIDesign/app/src/main/java/org/bts_netmind/guidesign/FirstActivity.java
@@ -0,0 +1,14 @@
+package org.bts_netmind.guidesign;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class FirstActivity extends Activity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first);
+    }
+}
