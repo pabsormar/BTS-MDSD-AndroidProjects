3ab5b8ea33fec5850eb5979089de75600afd3403
Pablo L <pabsormar@users.noreply.github.com>
2016-02-09 18:22:04 +0100

    First commit of the project 'GUIDesign' following IntelliJ structure.

---
+++ GUIDesign/app/src/main/java/org/bts_netmind/guidesign/FirstActivity.java
@@ -0,0 +1,17 @@
+package org.bts_netmind.guidesign;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class FirstActivity extends Activity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first);
+
+        final Button acceptBtn = (Button) this.findViewById(R.id.leftBtn);
+    }
+}
