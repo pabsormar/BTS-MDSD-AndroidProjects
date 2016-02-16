02e6efbaabc957d9e94f0e647beb541245b1bce7
Pablo L <pabsormar@users.noreply.github.com>
2016-02-16 13:38:42 +0100

    Clean 'DummyProject' instance

--- GUIDesign/app/src/main/java/org/bts_netmind/guidesign/FirstActivity.java
+++ GUIDesign/app/src/main/java/org/bts_netmind/guidesign/FirstActivity.java
@@ -3,13 +3,22 @@ package org.bts_netmind.guidesign;
 import android.app.Activity;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.RelativeLayout;
+
+import java.util.jar.Attributes;
 
 public class FirstActivity extends Activity
 {
+    RelativeLayout mRelLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first);
+
+        this.mRelLayout = (RelativeLayout) this.findViewById(R.id.relLayout);
+        Button mButton = new Button(this);
+        this.mRelLayout.addView(mButton);
     }
 }
