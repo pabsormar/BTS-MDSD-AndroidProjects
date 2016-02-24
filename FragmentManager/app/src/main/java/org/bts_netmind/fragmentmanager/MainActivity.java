13594f10570aec20c7fbb0cf5b399d35253391ec
Pablo L <pabsormar@users.noreply.github.com>
2016-02-24 13:01:22 +0100

    Last new amends to the project

--- FragmentManager/app/src/main/java/org/bts_netmind/fragmentmanager/MainActivity.java
+++ FragmentManager/app/src/main/java/org/bts_netmind/fragmentmanager/MainActivity.java
@@ -4,7 +4,6 @@ import android.app.Activity;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
-import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
 import android.util.Log;
