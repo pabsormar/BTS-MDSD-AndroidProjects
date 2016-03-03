63d4827e96dfe93016c021016e3a447f1eed65ef
Pablo L <pabsormar@users.noreply.github.com>
2016-03-03 23:47:02 +0100

    First commit of the project 'MenuManager' following IntelliJ structure.

---
+++ MenuManager/app/src/main/java/org/bts_netmind/menumanager/MainActivity.java
@@ -0,0 +1,117 @@
+package org.bts_netmind.menumanager;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+// extending from 'AppCompatActivity' allows to use the AppBar (formerly known as 'ActionBar') and the new 'Toolbar'
+public class MainActivity extends AppCompatActivity implements View.OnClickListener
+{
+    private static final String TAG_MAIN_ACTIVITY = "In-MainActivity";
+
+    private TextView hello_TxtView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar mToolbar = (Toolbar) this.findViewById(R.id.my_toolbar_main);
+        this.setSupportActionBar(mToolbar);
+
+        this.hello_TxtView = (TextView) this.findViewById(R.id.txtViewHello);
+            this.registerForContextMenu(this.hello_TxtView);
+        final View mainLayout = this.findViewById(R.id.layoutMainActivity);
+            mainLayout.setOnClickListener(this);
+    }
+
+    // This method is overridden to inflate the menu with a layout
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        MenuInflater mMenuInflater = this.getMenuInflater();
+        mMenuInflater.inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    // This method is overridden to provide functionality to menu items when clicked
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        Log.i(MainActivity.TAG_MAIN_ACTIVITY, "Menu item clicked");
+
+        if (item.getItemId() == R.id.copyItem)
+        {
+            Toast.makeText(this, "COPY item selected", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+        else if (item.getItemId() == R.id.cutItem)
+        {
+            Toast.makeText(this, "CUT item selected", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+        else if (item.getItemId() == R.id.pasteItem)
+        {
+            Toast.makeText(this, "PASTE item selected", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+        else if (item.getItemId() == R.id.searchItem)
+        {
+            Toast.makeText(this, "SEARCH item selected", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+        else if (item.getItemId() == R.id.compareItem)
+        {
+            Toast.makeText(this, "COMPARE item selected", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+        else
+            return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo)
+    {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        MenuInflater mContMenuInflater = this.getMenuInflater();
+        mContMenuInflater.inflate(R.menu.context_menu, menu);
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item)
+    {
+        if (item.getItemId() == R.id.likeItem)
+        {
+            Toast.makeText(this, "LIKE context item selected", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+        else if (item.getItemId() == R.id.dislikeItem)
+        {
+            Toast.makeText(this, "DISLIKE context item selected", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+        else
+            return super.onContextItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View whichView)
+    {
+        if (whichView.getId() == R.id.layoutMainActivity)
+        {
+            Log.i(MainActivity.TAG_MAIN_ACTIVITY, "Layout clicked");
+            this.startActivity(new Intent(this, SecondActivity.class));
+        }
+    }
+}
---
+++ MenuManager/app/src/main/java/org/bts_netmind/menumanager/SecondActivity.java
@@ -0,0 +1,142 @@
+package org.bts_netmind.menumanager;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.ActionMode;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.PopupMenu;
+import android.widget.Toast;
+
+public class SecondActivity extends AppCompatActivity implements View.OnClickListener, View.OnLongClickListener,
+                                                                        android.view.ActionMode.Callback,
+                                                                        PopupMenu.OnMenuItemClickListener
+{
+    private static final String TAG_SECOND_ACTIVITY = "In-SecondActivity";
+
+    private ActionMode mActionMode;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        Toolbar mToolbar = (Toolbar) this.findViewById(R.id.my_toolbar_second);
+        this.setSupportActionBar(mToolbar);
+
+        final View secondLayout = this.findViewById(R.id.layoutSecondActivity);
+            secondLayout.setOnClickListener(this);
+        final ImageView icon_ImgView = (ImageView) this.findViewById(R.id.imageViewAppIcon);
+            icon_ImgView.setOnLongClickListener(this);
+    }
+
+    @Override
+    public void onClick(View whichView)
+    {
+        if (whichView.getId() == R.id.layoutSecondActivity)
+        {
+            Log.i(SecondActivity.TAG_SECOND_ACTIVITY, "Layout clicked");
+            this.finish();
+        }
+    }
+
+    @Override
+    public boolean onLongClick(View whichView)
+    {
+        if (whichView.getId() == R.id.imageViewAppIcon)
+        {
+            Log.i(SecondActivity.TAG_SECOND_ACTIVITY, "ImageView long-clicked");
+            // if ActionMode is null "not started"
+            if (mActionMode != null) { return false; }
+            // Start the CAB
+            mActionMode = this.startActionMode(this);
+            whichView.setSelected(true);
+            return true;
+        }
+        else
+            return false;
+    }
+
+    // Called when the context menu is first created
+    @Override
+    public boolean onCreateActionMode(ActionMode mode, Menu menu)
+    {
+        MenuInflater mMenuInflater = mode.getMenuInflater();
+        mMenuInflater.inflate(R.menu.context_menu, menu);
+        return true;
+    }
+
+    // Nothing is done here, thus 'false' is returned by default
+    @Override
+    public boolean onPrepareActionMode(ActionMode mode, Menu menu)
+    {
+        return false;
+    }
+
+    @Override
+    public boolean onActionItemClicked(ActionMode mode, MenuItem item)
+    {
+        if (item.getItemId() == R.id.likeItem)
+        {
+            Toast.makeText(this, "LIKE item selected", Toast.LENGTH_SHORT).show();
+            mode.finish();
+            return true;
+        }
+        else if (item.getItemId() == R.id.dislikeItem)
+        {
+            Toast.makeText(this, "DISLIKE item selected", Toast.LENGTH_SHORT).show();
+            mode.finish();
+            return true;
+        }
+        else if (item.getItemId() == R.id.addItem)
+        {
+            Toast.makeText(this, "ADD item selected", Toast.LENGTH_SHORT).show();
+            // Create a pop-up menu for this option
+            PopupMenu mPopMenu = new PopupMenu(this, this.findViewById(R.id.addItem));
+            MenuInflater mMenuInflater = mPopMenu.getMenuInflater();
+            mMenuInflater.inflate(R.menu.popup_menu, mPopMenu.getMenu());
+            // The pop-up menu is configured to be clickable and later is shown
+            mPopMenu.setOnMenuItemClickListener(this);
+            mPopMenu.show();
+            //--------------
+            return true;
+        }
+        else
+            return false;
+    }
+
+    // Called when the Action mode is destroyed; the 'ActionMode' object is set to null
+    @Override
+    public void onDestroyActionMode(ActionMode mode) { mActionMode =  null; }
+
+    // This method handles the on-click events on the pop-up menu
+    @Override
+    public boolean onMenuItemClick(MenuItem item)
+    {
+        if (item.getItemId() == R.id.addOneItem)
+        {
+            Toast.makeText(this, "ADD-ONE item selected", Toast.LENGTH_SHORT).show();
+            if (item.isCheckable())
+            {
+                if (item.isChecked())
+                    item.setChecked(false);
+                else
+                    item.setChecked(true);
+            }
+            return true;
+        }
+        else if (item.getItemId() == R.id.addAllItem)
+        {
+            Toast.makeText(this, "ADD-ALL item selected", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+        else
+            return false;
+    }
+}
