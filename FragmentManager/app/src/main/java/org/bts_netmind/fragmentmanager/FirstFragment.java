979bec3d2c9915e37f57a7dd14ce47e844c1766b
Pablo L <pabsormar@users.noreply.github.com>
2016-04-18 15:10:33 +0200

    Some extra features

--- FragmentManager/app/src/main/java/org/bts_netmind/fragmentmanager/FirstFragment.java
+++ FragmentManager/app/src/main/java/org/bts_netmind/fragmentmanager/FirstFragment.java
@@ -1,6 +1,7 @@
 package org.bts_netmind.fragmentmanager;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.app.Fragment;
 import android.util.Log;
@@ -47,7 +48,32 @@ public class FirstFragment extends Fragment implements View.OnClickListener
         return fragView;
     }
 
+    // This is the actual 'onAttach' callback that should be used from now onwards. However, it it does not work very well with 'Fragment'
+    // Try using 'Fragment' from the support library
+    /*
+    @Override
+    public void onAttach(Context context)
+    {
+        super.onAttach(context);
+        Log.i(FirstFragment.TAG_FIRST_FRAGMENT, "onAttach()");
+
+        // This makes sure that the container activity has implemented
+        // the callback interface. If not, it throws an exception
+        if (context instanceof Activity)
+        {
+            try
+            {
+                Activity hostActivity = (Activity) context;
+                this.mFirstFragCallback = (OnFirstFragmentInterface) hostActivity;
+            }
+            catch(ClassCastException e)
+            { throw new ClassCastException(this.getActivity().toString() + " must implement OnFirstFragmentInterface"); }
+        }
+    }
+    */
+
     // Although this method is deprecated in API23, onAttach(Context context) does not work properly yet for previous Android versions
+
     @Override
     public void onAttach(Activity hostActivity)
     {
@@ -61,6 +87,7 @@ public class FirstFragment extends Fragment implements View.OnClickListener
         catch(ClassCastException e)
         { throw new ClassCastException(this.getActivity().toString() + " must implement OnFirstFragmentInterface"); }
     }
+
 
     @Override
     public void onActivityCreated(Bundle savedInstanceState)
