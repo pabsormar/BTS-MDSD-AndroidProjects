e0d4a0ef4937cf869ad3bb7263948dd305f402bd
Pablo L <pabsormar@users.noreply.github.com>
2016-06-18 17:34:41 +0200

    First commit of the project 'tddManager', following the IntelliJ Idea structure.

---
+++ tddManager/app/src/main/java/org/bts_netmind/tddmanager/MainActivity.java
@@ -0,0 +1,14 @@
+package org.bts_netmind.tddmanager;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
